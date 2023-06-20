f0dbe24042270f1c6ccdaa99b3b6a3180b61918f
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2023-06-20 14:15:37 +0300

    Cookie options modified 7

--- src/constants/tokenCookieOptions.ts
+++ src/constants/tokenCookieOptions.ts
@@ -4,6 +4,6 @@ export const REFRESH_TOKEN_COOKIE_OPTIONS: CookieOptions = {
   maxAge: 3600000 * 8, // Expires in 8 hours
   httpOnly: true,
   secure: true,
-  sameSite: "None",
+  sameSite: "none",
   domain: ".onrender.com",
 };
