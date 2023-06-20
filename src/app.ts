5bc00d02baa6c894d634ecb73ecbe95f7bce66ba
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2023-06-20 13:15:26 +0300

    Ready to deploy 2

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://speed-type-frontend.render.com",
+    origin: "https://speed-type-frontend.onrender.com",
     credentials: true,
   })
 );
